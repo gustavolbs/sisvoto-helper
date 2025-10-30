bb6f5ca31068006afa5921314364c7ab09455371
Gustavo Bispo <gustavo.luiz.bispo.santos@gmail.com>
2025-10-29 22:16:37 -0300

    third commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Papa from "papaparse";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
